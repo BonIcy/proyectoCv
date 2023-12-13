c3350f1ff67b4f75e2e30d5287a1a4130495b28c
asynkDF <124089647+asynkDF@users.noreply.github.com>
2023-12-12 19:49:54 -0500

    feat :construction.: Sidebar building

---
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';  
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../sidebar/sidebar.css';
+
+const Sidebar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="secondary" onClick={handleShow}>
+        <FaBars />
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Uwu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <hr />
+          <div>
+            <Link to="/managmentCampers" onClick={handleClose}>
+              Management Campers
+            </Link>
+          </div>
+          <div>
+            <Link to="/campers" onClick={handleClose}>
+              Campers
+            </Link>
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+};
+
+export default Sidebar;
