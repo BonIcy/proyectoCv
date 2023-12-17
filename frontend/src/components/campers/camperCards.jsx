65b62fcef2000a5912ed52586124756d85677305
asynkDF <124089647+asynkDF@users.noreply.github.com>
2023-12-16 19:23:09 -0500

    feat: :sparkles: English Level Filter
    filter for the english level 100% finished and functional in the campers section

--- frontend/src/components/campers/camperCards.jsx
+++ frontend/src/components/campers/camperCards.jsx
@@ -1,24 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Button } from 'react-bootstrap';
+import { Button, Form, Select } from 'semantic-ui-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CamperModal from '../campers/modal'; 
+import CamperModal from '../campers/modal';
 
 const Campers = () => {
   const [campers, setCampers] = useState([]);
-  const [showWorking, setShowWorking] = useState(false); 
+  const [showWorking, setShowWorking] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [selectedCamper, setSelectedCamper] = useState(null);
+  const [filterValue, setFilterValue] = useState(null);
+  const [englishLevelOptions, setEnglishLevelOptions] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:6929/cvs/Campers/WorkOrNot/${showWorking}`)
-      .then(response => {
+    const fetchData = async () => {
+      try {
+        let response;
+
+        if (filterValue === null) {
+          response = await axios.get(`http://localhost:6929/cvs/Campers/WorkOrNot/${showWorking}`);
+        } else {
+          response = await axios.post(`http://localhost:6929/cvs/Campers/SearchEngine`, {
+            EnglishLevel: filterValue,
+            $match: {
+              'EnglishLevelInfo._id': filterValue 
+            },
+          });
+        }
+
         setCampers(response.data);
+      } catch (error) {
+        console.error('Error fetching camper data:', error);
+      }
+    };
+
+    fetchData();
+  }, [showWorking, filterValue]);
+
+  useEffect(() => {
+    axios.get('http://localhost:6929/cvs/English_Levels/')
+      .then(response => {
+        const options = response.data.map(option => ({
+          key: option._id,
+          text: option.E_Name,
+          value: option._id,
+        }));
+        setEnglishLevelOptions(options);
       })
       .catch(error => {
-        console.error('Error fetching camper data:', error);
+        console.error('Error fetching English Levels data:', error);
       });
-  }, [showWorking]);
+  }, []);
 
   const openModal = (camper) => {
     setSelectedCamper(camper);
@@ -30,8 +62,22 @@ const Campers = () => {
     setSelectedCamper(null);
   };
 
+  const handleFilterChange = (e, data) => {
+    const value = data.value;
+    setFilterValue(value);
+  };
+
   return (
     <div>
+      <Form.Field>
+        <label>Filter by English Level:</label>
+        <Select
+          placeholder="Select English Level"
+          options={englishLevelOptions}
+          onChange={handleFilterChange}
+        />
+      </Form.Field>
+
       {campers.map(camper => (
         <div key={camper._id} className="card">
           <h2>{`${camper.Name} ${camper.LastName}`}</h2>
