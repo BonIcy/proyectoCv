99e94d2d01707dd49c43ddaba492c161b5e54f2d
asynkDF <124089647+asynkDF@users.noreply.github.com>
2023-12-18 01:43:32 -0500

    feat: :sparkles: Role authentication
    it works 100%, i think

--- frontend/src/components/auth/authService.jsx
+++ frontend/src/components/auth/authService.jsx
@@ -1,16 +1,23 @@
 const TOKEN_KEY = 'auth_token';
+const ROLE_KEY = 'user_role';
 
 const AuthService = {
-  login: (token) => {
+  login: (token, role) => {
     localStorage.setItem(TOKEN_KEY, token);
+    localStorage.setItem(ROLE_KEY, role);
   },
 
   logout: () => {
     localStorage.removeItem(TOKEN_KEY);
+    localStorage.removeItem(ROLE_KEY);
   },
 
   getToken: () => {
     return localStorage.getItem(TOKEN_KEY);
+  },
+
+  getRole: () => {
+    return localStorage.getItem(ROLE_KEY);
   },
 
   isLoggedIn: () => {
