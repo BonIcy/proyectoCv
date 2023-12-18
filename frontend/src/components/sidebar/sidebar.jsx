2121b6499cbf73dd301abeb9fd03b2410b9c9e48
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-12-17 19:31:07 -0500

    feat: :sparkles: improvement of the side bar, adding redirects and better styles
    improvement of the side bar, adding redirects and better styles

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -1,23 +1,55 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation  } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';  
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Sidebar = () => {
+  const navigate = useHistory();
   const [show, setShow] = useState(false);
-
   const closeSidebar = () => setShow(false);
   const showSidebar = () => setShow(true);
+  const goSignIn = () =>  navigate.push('/SignIn');
+  const goSignUp = () =>  navigate.push("/SignUp");
+
+  const location = useLocation();
+  const { state } = location;
+  
 
   return (
     <>
-      <Button variant="secondary" onClick={showSidebar}>
-        <FaBars />
-      </Button>
-
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{bgcolor: '#000087'}}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={showSidebar}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1,  fontFamily: 'Poppins', fontSize: '2rem'}}>
+              Working CampusLand
+            </Typography>
+            <Button color="inherit" style={{ fontFamily: 'Poppins', fontSize: '1rem'}} onClick={goSignIn}>
+              Sign In
+            </Button>
+            <Button color="inherit" style={{ fontFamily: 'Poppins', fontSize: '1rem'}} onClick={goSignUp}>
+              Sign Up
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Offcanvas show={show} onHide={closeSidebar}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Uwu</Offcanvas.Title>
