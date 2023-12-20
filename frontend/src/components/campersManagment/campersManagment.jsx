a31bb28ed8553f976481a2d5a85eee6d3f860fea
asynkDF <124089647+asynkDF@users.noreply.github.com>
2023-12-20 10:28:07 -0500

    style: :fire: remove some css code that breaks functions
    lol

--- frontend/src/components/campersManagment/campersManagment.jsx
+++ frontend/src/components/campersManagment/campersManagment.jsx
@@ -5,7 +5,6 @@ import CamperModal from '../campers/modal';
 import { Button, Form, Select, Card } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import { Link } from 'react-router-dom';
-import "./search.css";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
@@ -129,11 +128,6 @@ const CampersList = () => {
       <Form.Field>
         <label>Search by Name:</label>
         <div className='group'>
-          <svg viewBox="0 0 24 24" aria-hidden="true" class="icon">
-            <g>
-              <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-            </g>
-          </svg>
           <input
             type="text"
             placeholder="Enter camper name"
